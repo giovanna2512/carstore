e745bf2058d1821bd79f202b457dd6d22e05d78d
Giovanna Prates <uma.prates@gmail.com>
2023-09-27 21:46:32 -0300

    Adicionando o banco de dados

---
+++ src/main/java/br/com/carstore/model/Car.java
@@ -0,0 +1,20 @@
+package br.com.carstore.model;
+
+
+public class Car {
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
--- src/main/java/br/com/carstore/servlet/CreateCarServlet.java
+++ src/main/java/br/com/carstore/servlet/CreateCarServlet.java
@@ -1,5 +1,8 @@
 package br.com.carstore.servlet;
 
+import br.com.carstore.dao.CarDao;
+import br.com.carstore.model.Car;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,15 +10,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet
+
+@WebServlet("/create-car")
 public class CreateCarServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String carName = req.getParameter("car-name");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String carName = request.getParameter("car-name");
 
         System.out.println(carName);
+        Car car = new Car(carName);
+        new CarDao().createCar(car);
 
-        req.getRequestDispatcher("index.html").forward(req, resp);
+        request.getRequestDispatcher("index.html").forward(request, response);
+
     }
+
 }
